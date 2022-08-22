2c3ba9923dff25945151d6570911ee94a4479f78
Akshay Yadav <79329883+Akshayy57@users.noreply.github.com>
2022-08-22 20:07:15 +0530

    Add files via upload

---
+++ SwapStringWithOnly2Variable.java
@@ -0,0 +1,33 @@
+/*
+ * Swap Strings. Keep in mind that you do not have any additional space for more variables. Use just two variable.
+ * */
+
+package Internshala;
+
+import java.util.Scanner;
+
+public class SwapStringWithOnly2Variable {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter name1:");
+		String a = sc.nextLine();
+
+		System.out.println("Enter name2:");
+		String b = sc.nextLine();
+
+		System.out.println("Before swap => a: " + a + ", b: " + b);
+
+		// Swapping
+		a = a + b; // Add both string
+		b = a.substring(0, a.length() - b.length()); // select just the starting String
+		a = a.substring(b.length()); // select from second string index
+
+		System.out.println("String after Swap => a: " + a + ", b: " + b);
+		sc.close();
+
+	}
+
+}
